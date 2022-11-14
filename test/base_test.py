8f3f68485701ee1553585a43a0630345a62bd3b4
g-bhardwaj <12960909+g-bhardwaj@users.noreply.github.com>
2022-11-14 14:00:08 +0000

    Project setup and base framework.

---
+++ test/base_test.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+from pyspark.sql import SparkSession
+
+
+class ReusableSparkTestCase(TestCase):
+    spark: SparkSession = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.spark = SparkSession \
+            .builder \
+            .config("spark.sql.debug.maxToStringFields", "10000") \
+            .config("spark.sql.execution.arrow.pyspark.enabled", "true") \
+            .config("spark.sql.execution.arrow.pyspark.fallback.enabled", "true") \
+            .config("spark.sql.execution.arrow.maxRecordsPerBatch", "10000") \
+            .config("spark.sql.execution.arrow.pyspark.selfDestruct.enabled", "true") \
+            .config("spark.sql.shuffle.partitions", "7") \
+            .config("spark.driver.memory", "12g") \
+            .master("local[*]") \
+            .appName("SparkLocalTestCase") \
+            .getOrCreate()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.spark.stop()
