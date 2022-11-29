88471c0578893da8d43bd40ac939cd5da3df987a
Joe Fong <joe.fong@madetech.com>
2022-11-29 16:25:18 +0000

    move import statement to top

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,16 +1,15 @@
+import os
+import sys
+
+
 if __name__ == "__main__":
     """Run the pipeline process"""
 
-    import os
 
     if "DATABRICKS_RUNTIME_VERSION" in os.environ:
-        import sys
-
         sys.path.append("/dbfs/") # add /dbfs/ to path so that import statements works on databricks
 
-    from pipeline.jobs.fill_missing_months import (
-        fill_missing_months_for_transformed_spend,
-    )
+    from pipeline.jobs.fill_missing_months import fill_missing_months_for_transformed_spend
 
     fill_missing_months_for_transformed_spend(
         input_table_name="TransformedSpendData",
