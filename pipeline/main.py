a29dbbaa95c4132449d46b961262cf9a2476a2e0
Charlie Wren <cwren92@gmail.com>
2022-12-01 14:34:06 +0000

    Formatting

--- pipeline/main.py
+++ pipeline/main.py
@@ -5,11 +5,14 @@ import sys
 if __name__ == "__main__":
     """Run the pipeline process"""
 
-
     if "DATABRICKS_RUNTIME_VERSION" in os.environ:
-        sys.path.append("/dbfs/") # add /dbfs/ to path so that import statements works on databricks
+        sys.path.append(
+            "/dbfs/"
+        )  # add /dbfs/ to path so that import statements works on databricks
 
-    from pipeline.jobs.fill_missing_months import fill_missing_months_for_transformed_spend
+    from pipeline.jobs.fill_missing_months import (
+        fill_missing_months_for_transformed_spend,
+    )
 
     fill_missing_months_for_transformed_spend(
         input_table_name="TransformedSpendData",
