3e5702ace8d216a97720da9a4af3b21c421af941
Joe Fong <joe.fong@madetech.com>
2023-01-03 14:38:32 +0000

    add docstring to method

--- pipeline/jobs/run_forecast_and_output_to_blob.py
+++ pipeline/jobs/run_forecast_and_output_to_blob.py
@@ -25,20 +25,37 @@ logger = get_logger()
 
 
 def main():
+    """ An example workflow that can be run on local machine or databricks.
+
+    It will execute the following tasks:
+    1. Fetch latest spend data from Azure blob storage
+    2. Create 3 forecast models (SARIMA/ARIMA/ARMA) and compare their performance for each combination, base on the past spend data
+    3. Create actual forecast of a future period using the models which performed better for each combination
+    4. Output the forecast to blob storage.
+    """
     input_df = fetch_data_from_blob()
     models = model_choices()
     model_suggestions = compare_models_performance(input_df, models=models)
+
+    today = datetime.date.today()
+    next_month = today.replace(month = today.month + 1, day = 1) if today.month < 12 else today.replace(year = today.year + 1, month = 1, day = 1)
+
     forecast_df = run_forecast_with_suggested_models(
         input_df=input_df,
         model_suggestions=model_suggestions,
         models=models,
         months_to_forecast=12,
-        start_month=datetime.date(2023, 4, 1),
+        start_month=next_month
     )
     output_forecast_to_blob(forecast_df=forecast_df)
 
 
 def fetch_data_from_blob() -> pd.DataFrame:
+    """Fetch spend data from blob storage and prepare the data for feeding into model.
+
+    Returns:
+        A pandas dataframe of spend data which is summed up by month. For each combination, there is only one row for one month.
+    """
     logger.debug("loading spend data from Azure blob storage...")
 
     connect_spark_to_blob_storage()
@@ -48,15 +65,15 @@ def fetch_data_from_blob() -> pd.DataFrame:
     # select the combinations to make forecast for.
     category_list = [
         "Workforce Health & Education",
-        # "Document Management & Logistics",
-        # "Financial Services",
-        # "Network Services",
+        "Document Management & Logistics",
+        "Financial Services",
+        "Network Services",
     ]
     market_sector_list = [
         "Health",
         "Education",
-        # "Local Community and Housing",
-        # "Government Policy",
+        "Local Community and Housing",
+        "Government Policy",
     ]
 
     df = sdf.filter(
@@ -73,6 +90,11 @@ def fetch_data_from_blob() -> pd.DataFrame:
 
 
 def model_choices() -> list[ForecastModel]:
+    """ Instantiate the forecast models to use.
+
+    Returns: A list of ForecastModel object, each one represent a different type of forecast model.
+    """
+    
     logger.debug("Instantiating models...")
 
     sarima = SarimaModel(search_hyperparameters=True)
@@ -89,6 +111,16 @@ def model_choices() -> list[ForecastModel]:
 def compare_models_performance(
     input_df: pd.DataFrame, models: list[ForecastModel]
 ) -> pd.DataFrame:
+    """ Test train each model with 90% of spend data and compare their accuracy with the rest 10% data. 
+        Will output the result in a table format.
+
+    Args:
+        input_df (pd.DataFrame): A dataframe of prepared spend data.
+        models (list[ForecastModel]): A list of ForecastModel instances.
+
+    Returns:
+        pd.DataFrame: A table which contains the comparison result for each combination.
+    """
 
     logger.debug("Running test with past spend data to compare model performance...")
 
@@ -112,6 +144,18 @@ def run_forecast_with_suggested_models(
     months_to_forecast: int,
     start_month: datetime.date,
 ) -> pd.DataFrame:
+    """ Generate forecast for a future period with the better performed models for each combination.
+
+    Args:
+        input_df (pd.DataFrame): A dataframe of prepared spend data.
+        model_suggestions (pd.DataFrame): The output dataframe of method #compare_models_performance, which contains the information of which model performed better for which combination.
+        models (list[ForecastModel]): A list of ForecastModel instances.
+        months_to_forecast (int): The number of months to make forecast for.
+        start_month (datetime.date): The starting month to make forecast.
+
+    Returns:
+        pd.DataFrame: A dataframe of forecasted future spending.
+    """
 
     logger.debug("Generating forecast with suggested models...")
 
@@ -141,15 +185,18 @@ def run_forecast_with_suggested_models(
 
 
 def output_forecast_to_blob(forecast_df: pd.DataFrame):
+    """ Save the forecast data to blob storage.
+    """
 
     logger.debug("Saving forecast to blob storage...")
 
     start_month = min(forecast_df["SpendMonth"])
     end_month = max(forecast_df["SpendMonth"])
     start_month_in_yyyymmdd = start_month.strftime("%Y%m%d")
-    end_month_in_yyymmdd = end_month.strftime("%Y%m%d")
+    end_month_in_yyyymmdd = end_month.strftime("%Y%m%d")
+    today_in_yyyymmdd = datetime.date.today().strftime("%Y%m%d")
 
-    file_name = f"ForecastOutput/Spend_forecast_for_{start_month_in_yyyymmdd}-{end_month_in_yyymmdd}.csv"
+    file_name = f"ForecastOutput/Spend_forecast_for_{start_month_in_yyyymmdd}-{end_month_in_yyyymmdd}__forecast_made_on_{today_in_yyyymmdd}.csv"
 
     save_pandas_dataframe_to_blob(pandas_df=forecast_df, filename=file_name)
 
@@ -157,6 +204,5 @@ def output_forecast_to_blob(forecast_df: pd.DataFrame):
 
 
 if __name__ == "__main__":
-    """Run the pipeline process"""
 
     main()
