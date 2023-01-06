62ff95da59f98834b1438d16d29680cd5ba76fe5
Joe Fong <joe.fong@madetech.com>
2023-01-06 14:49:26 +0000

    add docstring and logging to method, update test to pytest format, make python script runnable

--- pipeline/jobs/get_active_combinations.py
+++ pipeline/jobs/get_active_combinations.py
@@ -1,28 +1,60 @@
 from pyspark.sql import DataFrame, functions as F
 import pandas as pd
+import datetime
 
-from pipeline.utils import connect_spark_to_blob_storage, load_latest_blob_to_pyspark
+from utils import get_logger
+from pipeline.utils import (
+    connect_spark_to_blob_storage,
+    load_latest_blob_to_pyspark,
+    save_pandas_dataframe_to_blob,
+)
 
 
-def filter_active_combinations(
+def get_active_combinations(
     customers: DataFrame,
     framework_category_pillar: DataFrame,
     spend_aggregated: DataFrame,
 ) -> list[tuple[str, str]]:
+    """From the given data, make a list of active Category / MarketSector combinations.
+    The current way of selecting active combinations:
+    For any framework that has status: 'Live' or 'Expired - Data Still Received',
+    find all spend record of such framework, and convert that to a unique list of Category / MarketSector.
+    The current implementation return the result in desc order of total spend amount.
 
-    active_frameworks = framework_category_pillar.filter(F.col('Status').isin("Live", "Expired - Data Still Received"))
-    active_framework_numbers = [row['FrameworkNumber'] for row in active_frameworks.collect()]
+    Args:
+        customers (DataFrame): Spark dataframe of dbo.Customers table
+        framework_category_pillar (DataFrame): Spark dataframe of dbo.FrameworkCategoryPillar table
+        spend_aggregated (DataFrame): Spark dataframe of dbo.SpendAggregated table
 
-    spends_of_active_frameworks = spend_aggregated.filter(spend_aggregated.FrameworkNumber.isin(active_framework_numbers))
+    Returns:
+        list[tuple[str, str]]: A list of string pairs, each pair representating a Category / MarketSector combination.
+    """
 
-    joined_spend = spends_of_active_frameworks.join(
-        other=customers, 
-        on=spends_of_active_frameworks.CustomerURN == customers.CustomerKey, 
-        how='left')
+    active_frameworks = framework_category_pillar.filter(
+        F.col("Status").isin("Live", "Expired - Data Still Received")
+    )
+    active_framework_numbers = [
+        row["FrameworkNumber"] for row in active_frameworks.collect()
+    ]
 
-    joined_spend = joined_spend.fillna({'MarketSector': 'Unassigned'})
+    spends_of_active_frameworks = spend_aggregated.filter(
+        spend_aggregated.FrameworkNumber.isin(active_framework_numbers)
+    )
 
-    active_combinations = joined_spend.select('Category', 'MarketSector').drop_duplicates()
+    joined_with_customer_table = spends_of_active_frameworks.join(
+        other=customers,
+        on=spends_of_active_frameworks.CustomerURN == customers.CustomerKey,
+        how="left",
+    ).fillna({"MarketSector": "Unassigned"})
+
+    sorted_by_spend_amount = (
+        joined_with_customer_table.groupby("Category", "MarketSector")
+        .agg(F.sum("EvidencedSpend"))
+        .sort(F.desc("sum(EvidencedSpend)"))
+    )
+    active_combinations = sorted_by_spend_amount.select(
+        "Category", "MarketSector"
+    ).drop_duplicates()
 
     output = [tuple(row) for row in active_combinations.collect()]
 
@@ -30,17 +62,43 @@ def filter_active_combinations(
 
 
 def main():
+    """The main method to be runned as a job. Will carry out below tasks:
+    1. Fetch relavant data tables from blob storage.
+    2. Filter out a list of active Category/Market Sector combination base on input data.
+    3. Save the result to blob storage.
+    """
+    logger = get_logger()
+    logger.debug("Started Job: Get active combinations")
+
+    logger.debug("Connecting to blob storage...")
     connect_spark_to_blob_storage()
 
-    customers = load_latest_blob_to_pyspark(table_name='Customers')
-    framework_category_pillar = load_latest_blob_to_pyspark(table_name='FrameworkCategoryPillar')
-    spend_aggregated = load_latest_blob_to_pyspark(table_name='SpendAggregated')
+    logger.debug("Loading data tables from blob storage")
+    customers = load_latest_blob_to_pyspark(table_name="Customers")
+    framework_category_pillar = load_latest_blob_to_pyspark(
+        table_name="FrameworkCategoryPillar"
+    )
+    spend_aggregated = load_latest_blob_to_pyspark(table_name="SpendAggregated")
 
-    active_combinations = filter_active_combinations(
+    logger.debug("Filtering active Category/MarketSector combinations based on data")
+    active_combinations = get_active_combinations(
         customers=customers,
         framework_category_pillar=framework_category_pillar,
-        spend_aggregated=spend_aggregated
+        spend_aggregated=spend_aggregated,
     )
 
-    df = pd.DataFrame(data=active_combinations, columns=['Category', 'MarketSector'])
-    
+    df = pd.DataFrame(data=active_combinations, columns=["Category", "MarketSector"])
+    today = datetime.date.today().strftime("%Y%m%d")
+    filename = f"ActiveCombinations/ActiveCombinations_{today}.csv"
+
+    logger.debug("Saving result to blob storage...")
+    save_pandas_dataframe_to_blob(pandas_df=df, filename=filename)
+
+    logger.debug(f"Saved to file: {filename}")
+    logger.debug("Job completed")
+
+
+if __name__ == "__main__":
+    """Run the pipeline process"""
+
+    main()
